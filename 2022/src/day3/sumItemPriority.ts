ffa9875ae6fbc4b44024155ffeb25e95cacdb704
Nicole <lancasternj@gmail.com>
2023-08-09 12:04:26 +0100

    completing test and implementation to find the sum of all the priority numbers for each rucksack

--- 2022/src/day3/sumItemPriority.ts
+++ 2022/src/day3/sumItemPriority.ts
@@ -1,3 +1,22 @@
 export const sumItemPriority = (commonRucksackItems: string[]) => {
-  return 0;
+  let sumOfItemPriorties = 0;
+
+  const charCodesOfItems = commonRucksackItems.map((item) => {
+    return item.charCodeAt(0);
+  });
+
+  const charCodeToPriorityNums = charCodesOfItems.map((charCode) => {
+    if (charCode >= 65 && charCode <= 90) {
+      charCode -= 38;
+    } else {
+      charCode -= 96;
+    }
+    return charCode;
+  });
+
+  const sumOfPriorties = charCodeToPriorityNums.forEach((priorityNum) => {
+    return (sumOfItemPriorties += priorityNum);
+  });
+
+  return sumOfItemPriorties;
 };
